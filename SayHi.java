9f0655c5e5d3b3abd9a2d6e0909f39167c62e958
Abderwa <awahni@njcu.edu>
2021-10-03 18:48:30 +0000

    first commit

---
+++ SayHi.java
@@ -0,0 +1,11 @@
+public class SayHi{
+	public static void main(String[] args){
+		System.out.println("Hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+	}
+}
+
+
